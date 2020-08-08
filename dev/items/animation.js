88ade98f00294fe6fe016562e5fad487f234a613
FOLDIKOS <64676942+FOLDIKOS@users.noreply.github.com>
2020-08-08 12:32:05 +0300

    Update animation.js

--- dev/items/animation.js
+++ dev/items/animation.js
@@ -106,8 +106,8 @@ Item.registerIconOverrideFunction(ItemID.ultimstew, function(item, texture){
 Item.setRequiresIconOverride(ItemID.ultimstew, true);
 
 Callback.addCallback("tick", function(){
-	var time = World.getThreadTime() % 6;
-	if (time == 0 || time == 6) {
+	var time = World.getThreadTime() % 2;
+	if (time == 0 || time == 2) {
 		if (twoBlinkIndex < 2) twoBlinkIndex++;
 		else twoBlinkIndex = 0;
 		if (threeBlinkIndex < 3) threeBlinkIndex++;
